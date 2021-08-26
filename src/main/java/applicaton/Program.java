6864e3167ad500f8c1776732e334ab8523a689f5
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-08-26 18:46:18 -0300

    Teste 3

--- src/main/java/applicaton/Program.java
+++ src/main/java/applicaton/Program.java
@@ -13,6 +13,7 @@ public class Program {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		system.out.println("Teste 2");
+		system.out.println("Teste 3");
 		em.getTransaction().begin();
 		em.remove(em.find(Person.class, 1));
 		em.getTransaction().commit();
